c55f0f6b09f37b15dd56a3b06d3fe6b446198815
daflamby <157586163+daflamby@users.noreply.github.com>
2025-03-25 22:54:06 +0100

    Update node.ts

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -66,58 +66,59 @@ export async function node(
 
   // POST /message - Receive messages from other nodes
   node.post("/message", (req, res) => {
-    if (currentState.killed) {
-      return res.status(400).send("Node is stopped");
+  if (currentState.killed) {
+    return res.status(400).send("Node is stopped");
+  }
+
+  const { senderId, value } = req.body;
+  if (senderId === nodeId) {
+    return res.status(400).send("Node cannot send messages to itself");
+  }
+
+  currentState.messages[senderId] = value;
+
+  // Check if enough messages have been received
+  if (Object.keys(currentState.messages).length >= N - F) {
+    let votes = { 0: 0, 1: 0 };
+    Object.values(currentState.messages).forEach((v) => {
+      if (v === 0) votes[0]++;
+      if (v === 1) votes[1]++;
+    });
+
+    if (votes[0] >= N - F) {
+      currentState.x = 0;
+      currentState.decided = true;
+    } else if (votes[1] >= N - F) {
+      currentState.x = 1;
+      currentState.decided = true;
+    } else {
+      currentState.x = "?";
+      currentState.decided = false;
     }
+  }
 
-    const { senderId, value } = req.body;
-    if (senderId === nodeId) {
-      return res.status(400).send("Node cannot send messages to itself");
-    }
-
-    currentState.messages[senderId] = value;
-
-    // Check if enough messages have been received
-    if (Object.keys(currentState.messages).length >= N - F) {
-      let votes = { 0: 0, 1: 0 };
-      Object.values(currentState.messages).forEach((v) => {
-        if (v === 0) votes[0]++;
-        if (v === 1) votes[1]++;
-      });
-
-      if (votes[0] >= N - F) {
-        currentState.x = 0;
-        currentState.decided = true;
-      } else if (votes[1] >= N - F) {
-        currentState.x = 1;
-        currentState.decided = true;
-      } else {
-        currentState.x = "?";
-        currentState.decided = false;
-      }
-    }
-
-    res.status(200).send("Message received");
-  });
+  return res.status(200).send("Message received");
+});
 
   // GET /start - Start the consensus algorithm
   node.get("/start", async (req, res) => {
-    if (currentState.killed) {
-      return res.status(400).send("Node is stopped");
+  if (currentState.killed) {
+    return res.status(400).send("Node is stopped");
+  }
+
+  console.log(`Node ${nodeId} starting consensus algorithm.`);
+  currentState.k = 1;
+
+  // Send initial value to other nodes
+  for (let i = 0; i < N; i++) {
+    if (i !== nodeId) {
+      await sendMessage(i, initialValue);
     }
+  }
 
-    console.log(`Node ${nodeId} starting consensus algorithm.`);
-    currentState.k = 1;
+  return res.status(200).send("Consensus started");
+});
 
-    // Send initial value to other nodes
-    for (let i = 0; i < N; i++) {
-      if (i !== nodeId) {
-        await sendMessage(i, initialValue);
-      }
-    }
-
-    res.status(200).send("Consensus started");
-  });
 
   // GET /stop - Stop the consensus algorithm
   node.get("/stop", (req, res) => {
